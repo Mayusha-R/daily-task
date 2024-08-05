426e15f0df5bfbe2c08e2519396266fe5f932fce
Mayusha-R <rathodmayusha@gmail.com>
2024-08-05 13:52:48 +0530

    Changed

---
+++ Day7/nodejs-project/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Kubernetes! Updated version.');
+});
+
+app.listen(port, () => {
+    console.log(`App running at http://localhost:${port}`);
+});
+
+app.get('/newroute', (req, res) => {
+    res.send('This is a new route!');
+});
